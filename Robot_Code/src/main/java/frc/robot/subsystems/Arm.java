f7e9da0da6216e0f58f2d261bcbcb4932ea1e9c3
Tazmania40 <tzupfer@gmail.com>
2019-01-14 18:02:54 -0500

    Commit End 1/14

--- Robot_Code/src/main/java/frc/robot/subsystems/Arm.java
+++ Robot_Code/src/main/java/frc/robot/subsystems/Arm.java
@@ -1,7 +1,48 @@
-package frc.subsystems;
+package frc.robot.subsystems;
+
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import edu.wpi.first.wpilibj.Talon;
+import frc.lib.loops.Loop;
 
 //TYLER'S REALM
-public class Arm {
+public class Arm extends Subsystem {
 
+    public static final Arm m_Arm = new Arm();
+    private TalonSRX armProx, armDist, armWrist;
+    private double armProxPower, armDistPower, armWristPower;
+    public static Arm getInstance() {return m_Arm;}
+
+    //LOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOP
+    private final Loop mloop = new Loop() {
+        @Override
+        public void onStart(double timestamp) {
+
+        }
+
+        @Override
+        public void onLoop(double timestamp) {
+
+        }
+
+        @Override
+        public void onStop(double timestamp) {
+
+        }
+    };
+
+
+    @Override
+    public void outputTelemetry() {
+    }
+
+    @Override
+    public void stop() {
+
+    }
+
+    @Override
+    public void reset() {
+
+    }
 }
 
