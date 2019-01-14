d06f5c511bd98eef1d6402b04225942ffd635c98
Milo <pamimoo@users.noreply.github.com>
2019-01-14 17:27:38 -0500

    IDK WHAT I AM DOING ANYMORE

--- Robot_Code/src/main/java/frc/robot/subsystems/Alien.java
+++ Robot_Code/src/main/java/frc/robot/subsystems/Alien.java
@@ -1,4 +1,6 @@
 package frc.robot.subsystems;
 
-public class Alien {
+public class Alien extends Subsystem{
+
+    private static Alien m_Alien = new Alien();
 }
--- Robot_Code/src/main/java/frc/subsystems/Alien.java
+++
@@ -1,5 +0,0 @@
-package frc.subsystems;
-
-public class Alien extends subsystem{
-
-}
