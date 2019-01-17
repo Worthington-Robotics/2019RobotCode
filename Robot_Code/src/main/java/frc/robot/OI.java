9b33e893146b30a7fc144b9eeccf87394dda4f58
MattSavage1340 <45183240+MattSavage1340@users.noreply.github.com>
2019-01-17 16:12:50 -0500

    BallManip

--- Robot_Code/src/main/java/frc/robot/OI.java
+++ Robot_Code/src/main/java/frc/robot/OI.java
@@ -8,6 +8,10 @@ import frc.lib.statemachine.Action;
 public class OI{
 
     public OI(){
+        Button second2 = new JoystickButton(Constants.SECOND, 2);
+        Button second3 = new JoystickButton(Constants.SECOND, 3);
+        second2.whileHeld(Action.toCommand(new ManipulatorAction(ManipulatorAction.ShotPower.PickUp)));
+        second3.whileHeld(Action.toCommand(new ManipulatorAction(ManipulatorAction.ShotPower.Shoot)));
         Button exit = new JoystickButton(Constants.MASTER, 1);
         exit.whenPressed(Action.toCommand(new StopMachineAction()));
     }
