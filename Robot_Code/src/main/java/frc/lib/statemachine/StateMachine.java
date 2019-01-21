aab0329ffb17e4f14bb041c77d807f1570f37c0e
Phillip White <boring.someone@gmail.com>
2019-01-21 12:16:55 -0500

    Changed Qed to Queued
    I don't know why

--- Robot_Code/src/main/java/frc/lib/statemachine/StateMachine.java
+++ Robot_Code/src/main/java/frc/lib/statemachine/StateMachine.java
@@ -7,16 +7,16 @@ import java.util.concurrent.ConcurrentLinkedQueue;
 public class StateMachine {
 
     private volatile static int state = -1;
-    private volatile static ConcurrentLinkedQueue<ActionGroup> qedStates;
+    private volatile static ConcurrentLinkedQueue<ActionGroup> queuedStates;
     private volatile static ActionGroup currentState;
     private static Runnable Man = () -> {
         state = 0;
         //smart dash stuff here.
-        if (qedStates == null) {
+        if (queuedStates == null) {
             state = -2;
         } else {
-            while (!qedStates.isEmpty()) {
-                currentState = qedStates.poll();
+            while (!queuedStates.isEmpty()) {
+                currentState = queuedStates.poll();
                 currentState.onStart();
                 while (!currentState.isFinnished()) {
                     currentState.onLoop();
@@ -30,7 +30,7 @@ public class StateMachine {
     };
 
     public static void runMan(StateMachineDescriptor Dis) {
-        qedStates = Dis.getStates();
+        queuedStates = Dis.getStates();
         Thread thread = new Thread(Man);
         thread.start();
 
--- Robot_Code/src/main/java/frc/lib/statemachine/StateMachineDescriptor.java
+++ Robot_Code/src/main/java/frc/lib/statemachine/StateMachineDescriptor.java
@@ -3,21 +3,21 @@ package frc.lib.statemachine;
 import java.util.concurrent.ConcurrentLinkedQueue;
 
 public class StateMachineDescriptor {
-    private ConcurrentLinkedQueue<ActionGroup> Qedstates;
+    private ConcurrentLinkedQueue<ActionGroup> Queuedstates;
 
     public StateMachineDescriptor() {
-        Qedstates = new ConcurrentLinkedQueue<>();
+        Queuedstates = new ConcurrentLinkedQueue<>();
     }
 
     public void addSequntal(Action action, long timeout_ms) {
-        Qedstates.add(new ActionGroup(action, timeout_ms));
+        Queuedstates.add(new ActionGroup(action, timeout_ms));
     }
 
     public void addParallel(Action[] action, long timeout_ms) {
-        Qedstates.add(new ActionGroup(action, timeout_ms));
+        Queuedstates.add(new ActionGroup(action, timeout_ms));
     }
 
     public ConcurrentLinkedQueue getStates() {
-        return Qedstates;
+        return Queuedstates;
     }
 }
