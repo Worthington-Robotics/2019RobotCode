67ff29453db462d4d179690e47aebd9091601c2f
Mark Fong <markcfong561@gmail.com>
2019-02-07 16:56:43 -0500

    Added Front of Cargo

--- Robot_Code/src/main/java/frc/robot/autoactiongroups/FrontOfCargo.java
+++ Robot_Code/src/main/java/frc/robot/autoactiongroups/FrontOfCargo.java
@@ -1,14 +1,11 @@
 package frc.robot.autoactiongroups;
 
-import frc.lib.geometry.State;
-import frc.lib.statemachine.ActionGroup;
 import frc.lib.statemachine.StateMachineDescriptor;
-import frc.lib.trajectory.Trajectory;
 import frc.robot.actions.DriveTra;
 import frc.robot.planners.TraGenerator;
 
-public class GoTenFeet extends StateMachineDescriptor {
-    public GoTenFeet() {
+public class FrontOfCargo extends StateMachineDescriptor {
+    public FrontOfCargo() {
 
         addSequential(new DriveTra(TraGenerator.getInstance().getTenFeet(), false), 30000 );
 
