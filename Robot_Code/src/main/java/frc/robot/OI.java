66030d9a60efa182eaf64d5131b9bf6b67f0a52a
Tyler Zupfer <tzupfer@gmail.com>
2019-02-10 22:22:18 -0500

    FIxy Fix: Massive overhaul and small changes

--- Robot_Code/src/main/java/frc/robot/OI.java
+++ Robot_Code/src/main/java/frc/robot/OI.java
@@ -11,33 +11,37 @@ import frc.robot.actions.ManipulatorAction;
 public class OI{
 
     public OI(){
-        Button second1 = new JoystickButton(Constants.LAUNCH_PAD, 1);
-        Button second2 = new JoystickButton(Constants.LAUNCH_PAD, 2);
-        Button second3 = new JoystickButton(Constants.LAUNCH_PAD, 3);
-        Button second4 = new JoystickButton(Constants.LAUNCH_PAD, 4);
-        Button second5 = new JoystickButton(Constants.LAUNCH_PAD, 5);
-        Button second6 = new JoystickButton(Constants.LAUNCH_PAD, 6);
-        Button second7 = new JoystickButton(Constants.LAUNCH_PAD, 7);
-        Button second8 = new JoystickButton(Constants.LAUNCH_PAD, 8);
-        Button second9 = new JoystickButton(Constants.LAUNCH_PAD, 9);
-        Button second10 = new JoystickButton(Constants.LAUNCH_PAD, 10);
-        Button second11 = new JoystickButton(Constants.LAUNCH_PAD, 11);
-        Button second12 = new JoystickButton(Constants.LAUNCH_PAD, 12);
-        Button second13 = new JoystickButton(Constants.LAUNCH_PAD, 13);
+        Button Launchpad1 = new JoystickButton(Constants.LAUNCH_PAD, 1);
+        Button Launchpad2 = new JoystickButton(Constants.LAUNCH_PAD, 2);
+        Button Launchpad3 = new JoystickButton(Constants.LAUNCH_PAD, 3);
+        Button Launchpad4 = new JoystickButton(Constants.LAUNCH_PAD, 4);
+        Button Launchpad5 = new JoystickButton(Constants.LAUNCH_PAD, 5);
+        Button Launchpad6 = new JoystickButton(Constants.LAUNCH_PAD, 6);
+        Button Launchpad7 = new JoystickButton(Constants.LAUNCH_PAD, 7);
+        Button Launchpad8 = new JoystickButton(Constants.LAUNCH_PAD, 8);
+        Button Launchpad9 = new JoystickButton(Constants.LAUNCH_PAD, 9);
+        Button Launchpad10 = new JoystickButton(Constants.LAUNCH_PAD, 10);
+        Button Launchpad11 = new JoystickButton(Constants.LAUNCH_PAD, 11);
+        Button Launchpad12 = new JoystickButton(Constants.LAUNCH_PAD, 12);
+        Button Launchpad13 = new JoystickButton(Constants.LAUNCH_PAD, 13);
+        Button Launchpad14 = new JoystickButton(Constants.LAUNCH_PAD,14);
+        Button Launchpad15 = new JoystickButton(Constants.LAUNCH_PAD,15);
+        Button Launchpad16 = new JoystickButton(Constants.LAUNCH_PAD,16);
 
-        second1.whileHeld(Action.toCommand(new AStopAction()));
-        second2.whileHeld(Action.toCommand(new ManipulatorAction(ManipulatorAction.ShotPower.PickUp)));
-        second3.whileHeld(Action.toCommand(new ManipulatorAction(ManipulatorAction.ShotPower.Shoot)));
-        second4.whileHeld(Action.toCommand(new AlienAction()));
-        second5.whileHeld(Action.toCommand(new AlienAction()));
-        second6.whileHeld(Action.toCommand(new AlienAction()));
-        second7.whileHeld(Action.toCommand(new AlienAction()));
-        second8.whileHeld(Action.toCommand(new AlienAction()));
-        second9.whileHeld(Action.toCommand(new AlienAction()));
-        second10.whileHeld(Action.toCommand(new AlienAction()));
-        second11.whileHeld(Action.toCommand(new AlienAction()));
-        second12.whileHeld(Action.toCommand(new AlienAction()));
-        second13.whileHeld(Action.toCommand(new AlienAction()));
+
+        /*Launchpad1.whileHeld(Action.toCommand(new ));
+        Launchpad2.whileHeld(Action.toCommand(new ));
+        Launchpad3.whileHeld(Action.toCommand(new ));
+        Launchpad4.whileHeld(Action.toCommand(new ));
+        Launchpad5.whileHeld(Action.toCommand(new ));
+        Launchpad6.whileHeld(Action.toCommand(new ));
+        Launchpad7.whileHeld(Action.toCommand(new ));
+        Launchpad8.whileHeld(Action.toCommand(new ));
+        Launchpad9.whileHeld(Action.toCommand(new ));
+        Launchpad10.whileHeld(Action.toCommand(new ));
+        Launchpad11.whileHeld(Action.toCommand(new ));
+        Launchpad12.whileHeld(Action.toCommand(new ));
+        Launchpad13.whileHeld(Action.toCommand(new ));*/
 
         Button anglePidButton = new JoystickButton(Constants.MASTER, 7);
         anglePidButton.whileHeld(Action.toCommand(new AnglePID()));
