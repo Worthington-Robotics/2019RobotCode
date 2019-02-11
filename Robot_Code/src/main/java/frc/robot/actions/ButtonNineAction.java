d8b415c04d17743c79719aca947eb9b365b39a2a
coalman321 <coalman321@users.noreply.github.com>
2019-02-10 21:57:59 -0500

    add back actions

---
+++ Robot_Code/src/main/java/frc/robot/actions/ButtonNineAction.java
@@ -0,0 +1,25 @@
+package frc.robot.actions;
+
+import frc.lib.statemachine.Action;
+
+public class ButtonNineAction extends Action {
+    @Override
+    public void onStart() {
+
+    }
+
+    @Override
+    public void onLoop() {
+
+    }
+
+    @Override
+    public boolean isFinished() {
+        return false;
+    }
+
+    @Override
+    public void onStop() {
+
+    }
+}
---
+++ Robot_Code/src/main/java/frc/robot/actions/ButtonOneAction.java
@@ -0,0 +1,26 @@
+package frc.robot.actions;
+
+import frc.lib.statemachine.Action;
+
+public class ButtonOneAction extends Action {
+    @Override
+    public void onStart() {
+
+    }
+
+    @Override
+    public void onLoop() {
+
+    }
+
+    @Override
+    public boolean isFinished() {
+        return false;
+    }
+
+    @Override
+    public void onStop() {
+
+    }
+}
+
