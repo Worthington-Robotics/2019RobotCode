44d2cf0ee22a4307e6c98f8b06f220f6e819f110
Mark Fong <markcfong561@gmail.com>
2019-02-15 20:11:18 -0800

    Add minimum threshold for angle pid

--- Robot_Code/src/main/java/frc/robot/subsystems/vision/AnglePIDOutput.java
+++ Robot_Code/src/main/java/frc/robot/subsystems/vision/AnglePIDOutput.java
@@ -6,8 +6,20 @@ import frc.lib.util.DriveSignal;
 import frc.robot.subsystems.Drive;
 
 public class AnglePIDOutput implements PIDOutput {
+
+    private static final double MIN_OUTPUT = 0.36;
+    private static final double THRESHOLD = 0.05;
+
     @Override
     public void pidWrite(double output) {
+        boolean negative = output < 0.0;
+        if (output != 0.0 && Math.abs(output) > THRESHOLD && Math.abs(output) < MIN_OUTPUT) {
+            output = MIN_OUTPUT;
+            if (negative) {
+                output *= -1;
+            }
+        }
+
         DriveSignal signal = new DriveSignal(output, -output,false);
         SmartDashboard.putNumber("vision/Drive Signal Output" , output);
         Drive.getInstance().setAnglePidLoop(signal);
